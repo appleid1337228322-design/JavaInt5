36e2947c92c28525cd726efd0ac0aa08f97da74f
Oleg <apple.id.1337228322@gmail.com>
2026-03-26 12:14:10 +0300

    services

---
+++ discovery-server/src/main/java/ru/project/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+
+package ru.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryserverApplication.class, args);
+    }
+
+}
